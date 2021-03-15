d9f7468dd14e1ba67cbe1d62f28f25fa455a1ef1
Your <you@example.com>
2021-03-15 21:52:14 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(braces, bracketsConfig) {
+  while(braces.length>0){
+  let minuspair = false
+  for (let i = 0; i < braces.length-1; i++){
+    for (let pair of bracketsConfig){
+      if (braces[i]==pair[0] && braces[i+1]==pair[1]){
+        braces = braces.slice(0,i)+braces.slice(i+2,braces.length)
+        minuspair = true
+        
+      }
+    
+    }
+
+  }
+  if (!minuspair && braces.length > 0){
+    return false
+  }
+  }
+  return true
+
+  };
+
